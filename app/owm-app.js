91d4a0708e4e788a18a1c1a819ff157b4c494409
LorenB <loren.barcus@gmail.com>
2014-06-30 21:07:26 -0400

    Updated bower components at fixed bugs.

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,32 +1,54 @@
-angular.module('OWMApp', ['ngRoute'])
-    .value('owmCities', 
-        ['New York', 'Dallas', 'Chicago'])
+angular.module('owmApp', ['ngRoute', 'ngAnimate'])
+    .value('owmCities', ['New York', 'Dallas', 'Chicago'])
+
+    .controller('HomeCtrl', function($scope) {
+            //empty for now
+        })
+
     .config(function($routeProvider){
         $routeProvider.when('/', {
             templateUrl : './home.html',
             controller : 'HomeCtrl'
-        })
-        .when('/cities/:city', {
-            templateUrl : './city.html',
-            controller : 'CityCtrl',
-            resolve : {
-                city: function(owmCities, $route, $location) {
-                    var city = $route.current.params.city;
-                    if(owmCities.indexOf(city) == -1 ) {
-                        $location.path('/error');
-                        return;
-                    }
-                    return city;
-                }
-            }
+        }).when('/cities/:city', {
+          templateUrl : './city.html',
+          controller : 'CityCtrl',
+          resolve : {
+              city: function(owmCities, $route, $location) {
+                  var city = $route.current.params.city;
+                  if(owmCities.indexOf(city) == -1 ) {
+                      $location.path('/error');
+                      return;
+                  }
+                  return city;
+              }
+          }
         })
         .when('/error', {
               template : '<p>Error Page Not Found</p>'
+        })
+        .otherwise({
+            redirectTo: '/error'
         });
     })
-    .controller('HomeCtrl', function($scope) {
-        //empty for now
-    })
+
     .controller('CityCtrl', function($scope, city) {
         $scope.city = city;
+    })
+    .run(function($rootScope, $location, $timeout){
+        $rootScope.$on('$rootChangeError', function() {
+            console.log("routChangeError...");
+            $location.path("/error");
+        });
+
+        $rootScope.$on('$routeChangeStart', function () {
+            console.log("routeChangeStart...");
+            $rootScope.isLoading = true;
+        });
+
+        $rootScope.$on('$routeChangeSuccess', function() {
+            console.log("routeChangeSuccess...");
+            $timeout(function() {
+                $rootScope.isLoading = false;
+            }, 1000);
+        });
     });
